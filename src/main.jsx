425396c455c4614865400513166406a973c9563d
muhammetyakac-cell <muhammetyakac@gmail.com>
2026-02-08 05:00:56 +0300

    Fix Vite React setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 // This is the main entry point of the application.
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);

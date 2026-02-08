870bc515418a582597a17b6e5b331195b9ec7447
muhammetyakac-cell <muhammetyakac@gmail.com>
2026-02-08 04:49:48 +0300

    Create src/pages/Fortunes.jsx file

---
+++ src/pages/Fortunes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Fortunes = () => {
+    return (
+        <div>
+            <h1>Your Fortunes</h1>
+            {/* Your fortune telling logic goes here */}
+        </div>
+    );
+};
+
+export default Fortunes;

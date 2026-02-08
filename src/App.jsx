aac7e7102d0bfee6ea3408e06780196143197767
muhammetyakac-cell <muhammetyakac@gmail.com>
2026-02-08 04:49:29 +0300

    Create App.jsx for the React app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Fortune Teller App!</h1>
+      <p>Get ready to discover your future.</p>
+    </div>
+  );
+}
+
+export default App;

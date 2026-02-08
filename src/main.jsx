07fa8c8ca072696c61d8a2fe9faa9a05652a10de
muhammetyakac-cell <muhammetyakac@gmail.com>
2026-02-08 05:08:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-// This is the main entry point of the application.
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Fortunes from './pages/Fortunes';
+import Appointments from './pages/Appointments';
+import BookingForm from './pages/BookingForm';
+import Dashboard from './pages/Dashboard';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gradient-to-br from-purple-900 via-purple-800 to-indigo-900">
+        <Navigation />
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/fortunes" element={<Fortunes />} />
+            <Route path="/appointments" element={<Appointments />} />
+            <Route path="/book/:fortuneTellerId" element={<BookingForm />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </AnimatePresence>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

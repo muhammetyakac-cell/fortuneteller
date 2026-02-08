54e9454a0db67ec2422517dc2497f066e3964da3
muhammetyakac-cell <muhammetyakac@gmail.com>
2026-02-08 04:49:35 +0300

    Create Appointments component

---
+++ src/pages/Appointments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Appointments = () => {
+    return (
+        <div>
+            <h1>Appointments Page</h1>
+            {/* Your appointment component logic goes here */}
+        </div>
+    );
+};
+
+export default Appointments;
